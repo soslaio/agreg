ac3899b3e8eb5acfad72025d5bea41f7b79272ca
rogerio <rogerio@pa.sebrae.com.br>
2020-06-29 11:04:13 -0300

    chore: ajuste da URL do banco de dados

--- prisitri/settings.py
+++ prisitri/settings.py
@@ -70,16 +70,10 @@ WSGI_APPLICATION = 'prisitri.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
